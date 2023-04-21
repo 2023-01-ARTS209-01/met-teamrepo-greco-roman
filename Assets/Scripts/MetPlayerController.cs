d0d61054f2b027adc81c0e84098e03949a6b2682
sfanzalone <sfanzalone@gmail.com>
2023-04-21 10:00:01 -0400

    Created a sample environment & player with working controller
    As placeholders, a rectangle is used for the floor, and a capsule resembles the player.  Also, a script I previously used for the metbase project to control the player was also used, and it all works.

---
+++ Assets/Scripts/MetPlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetPlayerController : MonoBehaviour
+{
+    [SerializeField] private float horizontalSpeed = 1f; //Changing this value INVERTS the controls
+    [SerializeField] private float verticalSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horMoveAmt = Input.GetAxis("Horizontal") * horizontalSpeed * Time.deltaTime; //Changing verticalSpeed INVERTS the controls
+        float verMoveAmt = Input.GetAxis("Vertical") * verticalSpeed * Time.deltaTime; //Changes vertical movement
+
+        transform.Rotate(0, 0, -horMoveAmt); //Making horMoveAmt POSITIVE will INVERTS the controls
+        //transform.Translate(0, verMoveSpeed, 0);
+        transform.Translate(0, verMoveAmt, 0);
+    }
+}
