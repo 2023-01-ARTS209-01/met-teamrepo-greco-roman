8a5f4ead3a2ecde2e559036594891f5117e59706
sfanzalone <sfanzalone@gmail.com>
2023-04-24 07:35:25 -0400

    Officially completed script; commented sections create errors
    The script has been officially completed.  However, if lines 24 - 36, and lines 65 - 68 are uncommented, an error message will appear within the console.  DON'T uncomment these lines until further notice!

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -21,7 +21,19 @@ public class Dialogue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //if (Input.GetMouseButtonDown(0))
+        //{
+            //if(textComponent.text == lines[index])
+            //{
+                //NextLine();
+            //}
+
+            //else
+            //{
+                //StopAllCoroutines();
+                //textComponent.text = lines[index];
+            //}
+        //}
     }
 
     void StartDialogue()
@@ -39,5 +51,20 @@ public class Dialogue : MonoBehaviour
             textComponent.text += c;
             yield return new WaitForSeconds(textSpeed);
         }
+    }
+
+    void NextLine()
+    {
+        if(index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+
+        //else
+        //{
+            //gameObject.setActive(false);
+        //}
     }
 }
