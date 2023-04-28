41a38cf5ab7d725cb95f90648ba432b7fd573c97
sfanzalone <sfanzalone@gmail.com>
2023-04-28 12:09:03 -0400

    Prevented animation from looping; will need to add a second one
    The SlideIn Animation Clip was originally looping, and now that aspect was disabled.  From here, an extra animation will need to be added to slide out when clicking "Ok".

--- Assets/Scripts/ImagePresenter.cs
+++ Assets/Scripts/ImagePresenter.cs
@@ -9,26 +9,16 @@ public class ImagePresenter : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D trigger)
     {
-        if (trigger.gameObject.CompareTag("Room") && atRoom)
+        if (trigger.gameObject.CompareTag("Environment") && atRoom)
         {
             atRoom = true;
 
             EnableBoolAnimator();
-
-            //public void NavigateTo(int scene)
-            //{
-                //Application.LoadLevel(scene); //Scene needs to be added - if I'm understanding it correctly.  Once added, this can be uncommented.
-            //}
         }
 
         if (trigger.gameObject.CompareTag("Environment") && !atRoom)
         {
             atRoom = false;
-
-            //public void Exitgame()
-            //{
-                Application.Quit();
-            //}
         }
     }
 
