529594b4c479aae6a85334464e4d285a7b31db15
sfanzalone <sfanzalone@gmail.com>
2023-04-28 11:05:25 -0400

    Updating script to include ability to support triggers

--- Assets/Scripts/ImagePresenter.cs
+++ Assets/Scripts/ImagePresenter.cs
@@ -4,33 +4,33 @@ using UnityEngine;
 
 public class ImagePresenter : MonoBehaviour
 {
-    private bool atExhibit = false;
+    private bool atRoom = false;
     
     private void OnTriggerEnter2D(Collider2D trigger)
     {
-        if (trigger.gameObject.CompareTag("Exhibit") && atExhibit)
+        if (trigger.gameObject.CompareTag("Room") && atRoom)
         {
-            atExhibit = true;
+            atRoom = true;
 
-            //public void DisableBoolAnimator(Animator anim)
-            //{
-            //anim.SetBool("IsDisplayed", false);
-            //}
+            public void DisableBoolAnimator(Animator anim)
+            {
+                anim.SetBool("IsDisplayed", false);
+            }
 
-            //public void DisableBoolAnimator(Animator anim)
-            //{
-                //anim.SetBool("IsDisplayed", true); Animation needs to be added.  Once added, this can be uncommented.
-            //}
+            public void DisableBoolAnimator(Animator anim)
+            {
+                anim.SetBool("IsDisplayed", true); //Animation needs to be added.  Once added, this can be uncommented.
+            }
 
             //public void NavigateTo(int scene)
             //{
-                //Application.LoadLevel(scene); Scene needs to be added - if I'm understanding it correctly.  Once added, this can be uncommented.
+                //Application.LoadLevel(scene); //Scene needs to be added - if I'm understanding it correctly.  Once added, this can be uncommented.
             //}
         }
 
-        if (trigger.gameObject.CompareTag("Environment") && !atExhibit)
+        if (trigger.gameObject.CompareTag("Environment") && !atRoom)
         {
-            atExhibit = false;
+            atRoom = false;
 
             //public void Exitgame()
             //{
